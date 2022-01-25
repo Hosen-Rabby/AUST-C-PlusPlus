01f4ebb91f9bbb377ab68881c67b4c29c2d8d2fa
Hosen-Rabby <mdhosen21018@gmail.com>
2022-01-25 08:12:31 +0600

    cpp files

---
+++ day1.cpp
@@ -0,0 +1,58 @@
+/*
+
+    int roll;
+    roll =14187;
+    cout<<roll<<endl;
+    cout<<"My roll is: "<<roll;
+
+    // cout<< roll<<endl;
+    // cout<< temparature<<endl;
+    // cout<< firstLetter<<endl;
+    // cout<< name<<endl;
+*/
+
+#include <iostream>
+using namespace std;
+
+
+
+// main function
+int main() {
+
+    // data type
+    // int, double, char,string, boolean, float
+
+    // int roll = 121;
+    // double temparature = 22.5;
+    // char firstLetter = 'H';
+    // string name = "Md. Hosen";
+    // bool istrue = true;
+
+ 
+
+    int num1 = 12;
+    int num2 = 5;
+
+    // int result = num1 + num2;
+    // int result = num1 - num2;
+    // int result = num1 * num2;
+    // int result = num1 / num2;
+    // int result = num1 % num2;
+    // cout<<num1;
+
+
+    // string nam;
+    // cout<<"Enter your name: ";
+
+    // cin>>nam;
+    //  or   
+    // getline(cin, nam);
+    // cout<<"My name is: "<<nam;
+
+    
+    
+
+    // cout<<result;
+
+
+} 
---
+++ day2.cpp
@@ -0,0 +1,83 @@
+/*
+
+
+ // operator
+    +,_,*,/
+
+
+    assignment operator
+
+    = , += , *= , /=, 
+    
+
+    firstNumber = 12;
+    firstNumber+=3;
+    firstNumber = firstNumber + 3;
+    firstNumber-=3;
+    firstNumber = firstNumber - 3;
+
+    cout<<firstNumber;
+
+
+    comparison
+
+    ==
+    !=
+    >
+    <
+    >=
+    <=
+
+
+   int firstNumber, secondNumber;
+    firstNumber = 3;
+    secondNumber = 5;
+
+    cout<< (firstNumber == secondNumber);
+    cout<< (firstNumber != secondNumber);
+    cout<< (firstNumber > secondNumber);
+    cout<< (4 <= 3);
+
+
+
+    logical operator
+ 
+    &&
+    ||
+    !
+  
+    int number = 5;
+    cout<< (number < 10 && number > 6);
+    cout<< (number < 3 || number > 6);
+    cout<< (!(number < 3 || number > 6));
+ 
+    max()
+    min()
+    floor()
+    ceil()
+    round()
+    sqrt()
+    abs()
+   
+
+    cout<< max(50, 10);
+    cout<< min(50, 10);
+    cout<< floor(5.9);
+    cout<< ceil(5.1);
+    cout<< round(5.5);
+    cout<<sqrt(4);
+    cout<<abs(-12);
+*/  
+
+
+#include <iostream>
+
+#include<cmath>
+using namespace std;
+
+int main() {  
+
+
+
+
+} 
