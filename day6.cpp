f8b869fee74ad2a64be16dc3a3ded060e0d4e8c6
Hosen-Rabby <mdhosen21018@gmail.com>
2022-02-06 22:27:32 +0600

    day 6

---
+++ day6.cpp
@@ -0,0 +1,60 @@
+/*
+    int numbers[] = {12, 5,6};
+    
+    int arrLen = sizeof(numbers) / sizeof(numbers[0]);
+                     
+    
+    cout<< arrLen;
+    
+    string names[] = {"Puspita", "Zarin", "Afrain"};
+    cout<< names[0]<<endl<<names[1];
+    
+        int index = 0;
+    do{
+        cout<< index<< endl;
+        index--;
+    }while(index > 1);
+    cout<<"Loop end";
+    
+    
+    
+      string names="Habiba";
+    cout<< names.size();
+    
+    
+        
+    string name = "Nafisa";
+    cout<< name.size()<<endl;
+    cout<< name.length()<<endl;
+    
+    
+        string names[] = {"Puspita", "Habiba", "Zarin", "Nafisa", "Afrain"};
+    int arrLen = sizeof(names) / sizeof(names[0]);
+    
+    // cout<< sizeof(names);
+    
+    for(int i = 0; i < arrLen; i++){
+        cout<< "Hello "<<names[i]<<endl;
+    }
+
+*/ 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // loop
+    
+    /*
+    while
+    do while
+    for
+    */ 
+    
+    
+    
+
+    
+}
