b3feb1e6b61a2420e9e94d88df5145d913020fb1
Hosen-Rabby <mdhosen21018@gmail.com>
2022-02-09 21:39:18 +0600

    day7

---
+++ day7.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+
+// int addition(int num1, int num2){
+//     int sum = num1 + num2;
+//     // cout<< sum;
+//     return sum;
+// }
+
+// string grettinng(string name)
+// {
+//     cout<<"Hello, "<<name<<endl;
+// }
+
+// int sum(int num1, int num2, int num3 = 0){
+//     int summ = num3 + num2 + num1;
+//     cout<< summ;
+// }
+
+
+void myFunc(string name, int age){
+    cout<<"Hello "<<name<<endl;
+    cout<<"You are "<<age << " years old";
+}
+
+
+
+
+int main()
+{
+    // addition(2, 5);
+    // cout<<addition(2, 6);
+    // grettinng("Puspita");
+    // grettinng("Nafisa");
+    
+    
+    // sum(2,3);
+    
+    
+    myFunc("Maliha", 20);
+    
+    
+    return 0;
+    
+}
